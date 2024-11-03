1b85b76fe6641b164102908d044b194f7c0f6117
Zack <168221270+Zval321@users.noreply.github.com>
2024-11-03 12:39:08 -0600

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
+import { createBrowserRouter, RouterProvider, Outlet} from "react-router-dom";
 import { Page } from './Page.jsx'
 import './App.css'
+import { Header } from './Header.jsx'
 
 function App() {
+  const router = createBrowserRouter([
+    { 
+      element: (
+        <div>
+          <Outlet />
+        </div>
+      ), 
+      children: [
+        {
+          path: '/Page', 
+          element: <Page/>
+        }, 
+        { 
+          path: '/', 
+          element: <Header />
+        }
+      ]
+    }
+  ])
+
 
   return (
-    <div>
-      <Page />
-    </div>
+    <RouterProvider router={router} />
   )
 }
 
--- src/Page.jsx
+++ src/Page.jsx
@@ -1,27 +1,18 @@
-import { PDFDocument, StandardFonts, rgb } from 'pdf-lib';
+import React from 'react';
+import { Creator } from './Creator'
 
 
-export async function Page() {
-  const pdfDoc = await PDFDocument.create();
-  const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman);
+export function Page () { 
 
-  const page = pdfDoc.addPage();
-  const { width, height } = page.getSize();
-  const fontSize = 30;
-  page.drawText('Creating PDFs in JavaScript is awesome!', {
-    x: 50,
-    y: height - 4 * fontSize,
-    size: fontSize,
-    font: timesRomanFont,
-    color: rgb(0, 0.53, 0.71),
-  });
-
-  const pdfBytes = await pdfDoc.save();
-
-  // Create a Blob from the PDF and download it
-  const blob = new Blob([pdfBytes], { type: 'application/pdf' });
-  const link = document.createElement('a');
-  link.href = URL.createObjectURL(blob);
-  link.download = 'my_document.pdf';
-  link.click();
+    const handleDownload = async () => {
+      await Creator(); // Call the PDF creation function
+    };
+  return ( 
+    <div>
+      <h1>
+        Download Your PDF Below: 
+      </h1>
+      <button onClick={handleDownload}>Download</button>
+    </div>
+  )
 }
